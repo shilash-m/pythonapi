1828affce912c44b79ab6fe76717db608f6e2cac
shilash-m <78814577+shilash-m@users.noreply.github.com>
2021-07-03 14:32:32 +0530

    Update finalapi1.py

--- finalapi1.py
+++ finalapi1.py
@@ -4,7 +4,7 @@ from flask_jwt import JWT, jwt_required
 from firstapi.finalapi3 import autentication, identity
 from resources.userresources import Userregister
 from resources.item_database import Item, Itemlist
-from d import db
+
 
 from resources.model_resourcde import Modell,Storelist
 from models.store_model import StoreModel
@@ -17,9 +17,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///database1.db"
 app.secret_key = "shi"
 db.init_app(app)
-@app.before_first_request
-def create_table():
-    db.create_all()
+
 
 
 jwt = JWT(app, autentication, identity)
@@ -35,5 +33,9 @@ api.add_resource(Storelist,"/stores")
 
 if __name__ == "__main__":
     app.run(port=1000)
+    from d import db
+    @app.before_first_request
+    def create_table():
+        db.create_all()
 
 
